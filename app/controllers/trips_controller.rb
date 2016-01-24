18e13d947638d080cdb71ab6bbe52becf8de3d4d
mllehrer <mia.lehrer@gmail.com>
2016-01-23 19:13:56 -0800

    start working with uber api

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,12 +4,23 @@ class TripsController < ApplicationController
     if current_user
       @trips = current_user.trips
     end
-    # show the all the places/events and transit between them in the
   end
 
   def show
     @trip = Trip.find(params[:trip_id])
     @places = current_user.trips.find(params[:trip_id]).trip_events
+    @client = Uber::Client.new do |config|
+  config.server_token  = "YOUR_SERVER_TOKEN"
+  end
+  @legs =
+    @places.each_with_index do |place, i|
+       @start_loc = get_place_location(place[i].place_id)
+      @end_loc = get_place_location(place[i+1].place_id)
+
+      time = @client.time_estimations(start_latitude: @start_loc[:latitude], start_longitude: @start_loc[:longitude], end_latitude: @end_loc[:latitude], end_longitude: @end_loc[:longitude])
+
+      price =  @client.price_estimations(start_latitude: @start_loc[:latitude], start_longitude: @start_loc[:longitude], end_latitude: @end_loc[:latitude], end_longitude: @end_loc[:longitude])
+    end
   end
 
   def create
--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,6 +1,35 @@
 module TripsHelper
   def get_place_info(place_id)
-    @client = GooglePlaces::Client.new(GOOGLE_API_KEY)
+    @client = GooglePlaces::Client.new(ENV["GOOGLE_API_KEY"])
     return @client.spot(place_id)
   end
+
+  #for api calls
+  def get_place_location(place_id)
+    @client = GooglePlaces::Client.new(ENV["GOOGLE_API_KEY"])
+    return { latitude: @client.spot(lat), longitude: @client.spot(lng) }
+  end
+
+  def get_uber_info
+  @client = Uber::Client.new do |config|
+  config.server_token  = ENV["UBER_SERVER_TOKEN"]
+  config.client_id     = ENV["UBER_CLIENT_ID"]
+  config.client_secret = ENV["UBER_CLIENT_SECRET"]
+  end
+
+  def request_uber_price
+    @client = Uber::Client.new do |config|
+    config.server_token  = ENV["UBER_SERVER_TOKEN"]
+    end
+    @client.price_estimations(start_latitude: @start[:latitude], start_longitude: @start[:longitude], end_latitude: @end[:latitude], end_longitude: @end[:longitude])
+  end
+
+  def request_uber_time
+  @client = Uber::Client.new do |config|
+  config.server_token  = "YOUR_SERVER_TOKEN"
+  end
+  @client.time_estimations(start_latitude: @start_loc[:latitude], start_longitude: @start_loc[:longitude], end_latitude: @end_loc[:latitude], end_longitude: @end_loc[:longitude])
+  end
 end
+end
+
