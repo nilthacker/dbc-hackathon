cbbeb4d4677a3e7aa17e3ede550c64384b19647b
Nil Thacker <nil@nil.gs>
2016-01-23 16:23:13 -0800

    Update seeds file for dropdown categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,19 +4,13 @@ low_intensity = User.create(email: "test@test.com", password: "password")
 # create a trip for now
 low_intensity.trips.create(date: Time.now)
 
-# add breakfast, lunch, and dinner to this new trip
+# add trip events
 low_intensity.trips.first.trip_events.create(event_type: "breakfast", place_id: "ChIJKye2O32AhYARG37tXAsOj7Q")
+low_intensity.trips.first.trip_events.create(event_type: "landmark", place_id: "ChIJiZyN7IeAhYARfJshHJaQckc")
 low_intensity.trips.first.trip_events.create(event_type: "lunch", place_id: "ChIJSToWqn2AhYAR1CuH2iGMvuo")
+low_intensity.trips.first.trip_events.create(event_type: "arts", place_id: "ChIJ53I1Yn2AhYAR_Vl1vNygfM")
 low_intensity.trips.first.trip_events.create(event_type: "dinner", place_id: "ChIJaZ6Hg4iAhYARrwVEfkv9Zlw")
-
-# add after breakfast event
-low_intensity.trips.first.trip_events.create(event_type: "park", place_id: "ChIJiZyN7IeAhYARfJshHJaQckc")
-
-# add after lunch event
-low_intensity.trips.first.trip_events.create(event_type: "museum", place_id: "ChIJ53I1Yn2AhYAR_Vl1vNygfM")
-
-# add after dinner event
-low_intensity.trips.first.trip_events.create(event_type: "movie", place_id: "ChIJ-zhtDYeAhYAREh_HF8J91xQ")
+low_intensity.trips.first.trip_events.create(event_type: "culture", place_id: "ChIJ-zhtDYeAhYAREh_HF8J91xQ")
 
 # ===========================
 
@@ -25,19 +19,13 @@ high_intensity = User.create(email: "test2@test.com", password: "password")
 # create a hith entensity trip for now
 high_intensity.trips.create(date: Time.now)
 
-# add breakfast, lunch, and dinner to this new trip
+# add trip events
 high_intensity.trips.first.trip_events.create(event_type: "breakfast", place_id: "ChIJF7AFf-SAhYARJPDwj0CBm14")
-high_intensity.trips.first.trip_events.create(event_type: "lunch", place_id: "ChIJZTCRmjyHhYAR1aeqChJLjng")
-high_intensity.trips.first.trip_events.create(event_type: "dinner", place_id: "ChIJHSGzi_yAhYARptfivXk-Qlo")
-
-# add after breakfast events
-high_intensity.trips.first.trip_events.create(event_type: "park", place_id: "ChIJQ09tqOGAhYARinfjX2EOfaM")
+high_intensity.trips.first.trip_events.create(event_type: "landmark", place_id: "ChIJQ09tqOGAhYARinfjX2EOfaM")
 high_intensity.trips.first.trip_events.create(event_type: "landmark", place_id: "ChIJvXl2weCAhYARjSxnhzrBfNc")
-
-# add after lunch events
-high_intensity.trips.first.trip_events.create(event_type: "museum", place_id: "ChIJIUT7rEOHhYARucp3wM")
-high_intensity.trips.first.trip_events.create(event_type: "park", place_id: "ChIJW5w2V2iHhYARQoOl_ZsQlOg")
-
-# add after dinner events
-high_intensity.trips.first.trip_events.create(event_type: "park", place_id: "ChIJHSGzi_yAhYARnrPmDWAx9ro")
+high_intensity.trips.first.trip_events.create(event_type: "lunch", place_id: "ChIJZTCRmjyHhYAR1aeqChJLjng")
+high_intensity.trips.first.trip_events.create(event_type: "landmark", place_id: "ChIJIUT7rEOHhYARucp3wM")
+high_intensity.trips.first.trip_events.create(event_type: "landmark", place_id: "ChIJW5w2V2iHhYARQoOl_ZsQlOg")
+high_intensity.trips.first.trip_events.create(event_type: "dinner", place_id: "ChIJHSGzi_yAhYARptfivXk-Qlo")
+high_intensity.trips.first.trip_events.create(event_type: "landmark", place_id: "ChIJHSGzi_yAhYARnrPmDWAx9ro")
 high_intensity.trips.first.trip_events.create(event_type: "landmark", place_id: "ChIJAQAAQIyAhYARRN3yIQG4hd4")
