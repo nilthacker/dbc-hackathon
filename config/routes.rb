094d497cfb80988651d45b8d0b0955ba8fffbfae
Nil Thacker <nil@nil.gs>
2016-01-23 16:08:08 -0800

    Placeholder trip index and view controller actions. Start place view partial.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'trips#index'
+
+  post '/trips' => 'trips#create'
+  get '/trips' => 'trips#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
