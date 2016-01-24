ab8bf0aa658120f6b263fa8591df287c0b63280e
mllehrer <mia.lehrer@gmail.com>
2016-01-23 21:19:57 -0800

    MVP. Show uber estimates between events

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   get 'trips/' => 'trips#index'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
+  get '/test_trip' => 'trips#test_request_uber_time'
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
